2871eaa3f9232638d5fe231e43149e091956a90b
Luciano <lnlupo95@gmail.com>
2023-01-19 12:48:32 -0300

    19/01/2023 Video: 11

--- app/application.go
+++ app/application.go
@@ -1,14 +1,14 @@
 package app
 
 import (
-	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin" //El paquete gin es un framework Web para poder usar HTTP
 )
 
 var (
-	router = gin.Default()
+	router = gin.Default() //Crea un Router
 )
 
 func StartApplication() {
-	mapUrls()
-	router.Run(":8080")
+	mapUrls()           //Trae todas las rutas almacenadas en url_mapping.go
+	router.Run(":8080") //Corre el Router en el Puerto 8080
 }
--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -5,6 +5,12 @@ import (
 	"github.com/LucianoLupo95/bookstore_users-api/datasources/mysql/users_db"
 	"github.com/LucianoLupo95/bookstore_users-api/utils/date_utils"
 	"github.com/LucianoLupo95/bookstore_users-api/utils/errors"
+	"strings"
+)
+
+const (
+	indexUniqueEmail = "email_UNIQUE"
+	queryInsertUser = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?, ?, ?, ?);"
 )
 
 var (
@@ -27,14 +33,24 @@ func (user *User) Get() *errors.RestErr {
 	return nil
 }
 func (user *User) Save() *errors.RestErr {
-	currentUser := usersDB[user.Id]
-	if currentUser != nil {
-		if currentUser.Email == user.Email {
-			return errors.NewBadRequestError(fmt.Sprintf("Email %s already registered", user.Email))
-		}
-		return errors.NewBadRequestError(fmt.Sprintf("User %d already exists", user.Id))
+	stmt, err := users_db.Client.Prepare(queryInsertUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
+	defer stmt.Close()
 	user.DateCreated = date_utils.GetNowString()
-	usersDB[user.Id] = user
+	insertResult, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
+	if err != nil {
+		if strings.Contains(err.Error(), indexUniqueEmail) {
+			return errors.NewBadRequestError(fmt.Sprintf("Email %s already exists: %s", err.Error()"")
+		}
+		return errors.NewInternalServerError(fmt.Sprintf("Error when trying to save user: %s", user.Email))
+	}
+
+	userId, err := insertResult.LastInsertId()
+	if err != nil {
+		return errors.NewInternalServerError(fmt.Sprintf("Error when trying to save user: %s", err.Error()))
+	}
+	user.Id = userId
 	return nil
 }
