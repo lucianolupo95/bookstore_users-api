38425d4bec2c023512f169f8542f230b37d445c1
Luciano <lnlupo95@gmail.com>
2023-01-19 10:48:13 -0300

    19/01/2023 Video 11

--- datasources/mysql/users_db/users_db.go
+++ datasources/mysql/users_db/users_db.go
@@ -5,27 +5,38 @@ import (
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
+	"os"
+)
+
+const (
+	mysql_users_username = "mysql_users_username"
+	mysql_users_password = "mysql_users_password"
+	mysql_users_host     = "mysql_users_host"
+	mysql_users_schema   = "mysql_users_schema"
 )
 
 var (
-	Client *sql.DB
+	Client   *sql.DB
+	username = os.Getenv(mysql_users_username)
+	password = os.Getenv(mysql_users_password)
+	host     = os.Getenv(mysql_users_host)
+	schema   = os.Getenv(mysql_users_schema)
 )
 
 func init() {
 	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8",
-		"root",
-		"mysql",
-		"127.0.0.1:3306",
-		"users_db",
+		username,
+		password,
+		host,
+		schema,
 	)
 	var err error
-	Client, err := sql.Open("mysql", dataSourceName)
+	Client, err = sql.Open("mysql", dataSourceName)
 	if err != nil {
 		panic(err)
 	}
 	if err := Client.Ping(); err != nil {
 		panic(err)
 	}
-
 	log.Println("Database succesfully configured")
 }
